309836546da5330a8b435ce044d6f2d10cfb2f1b
THEeSH.io <admin@eshaman.io>
2025-08-26 16:29:57 -0700

    Add Apollo server API route

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,14 @@
+import { typeDefs } from '../../../graphql/schema';
+import { resolvers } from '../../../graphql/resolvers';
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const handler = startServerAndCreateNextHandler(server);
+
+export const GET = handler;
+export const POST = handler;
